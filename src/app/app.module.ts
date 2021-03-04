cb5dfd2e4b62a5cd66e61c2ad4907a7bf672c287
Erik Verduin <everduin94@gmail.com>
2021-03-04 12:32:09 -0600

    updated to use angular fire, may still need to explicitly set angularfire as deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,25 @@ import { HotToastModule, HotToastService } from "@ngneat/hot-toast";
 import { TippyModule } from "@ngneat/helipopper";
 import { HotkeysModule } from "@ngneat/hotkeys";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { CopyToClipboardModule, COPY_TO_CLIPBOARD_HANDLER } from "@ngneat/copy-to-clipboard";
+import {
+  CopyToClipboardModule,
+  COPY_TO_CLIPBOARD_HANDLER,
+} from "@ngneat/copy-to-clipboard";
 import copy from "copy-to-clipboard";
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatInputModule } from "@angular/material/input";
 import { NG_ENTITY_SERVICE_CONFIG } from "@datorama/akita-ng-entity-service";
 import { ReactiveFormsModule } from "@angular/forms";
+import { AngularFireModule } from "@angular/fire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtPopNaDI1KRwq1mrM1Ya_VOgndq3HsQ0",
+  authDomain: "erxk-article-playground.firebaseapp.com",
+  projectId: "erxk-article-playground",
+  storageBucket: "erxk-article-playground.appspot.com",
+  messagingSenderId: "772954248415",
+  appId: "1:772954248415:web:f17997769b3987140e456a",
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,12 +41,11 @@ import { ReactiveFormsModule } from "@angular/forms";
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    !environment.production
-      ? HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabaseService, {
-          delay: 0,
-          passThruUnknownUrl: true,
-        })
-      : [],
+    AngularFireModule.initializeApp(firebaseConfig),
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabaseService, {
+      delay: 0,
+      passThruUnknownUrl: true,
+    }), // Always on!
     environment.production ? [] : AkitaNgDevtools.forRoot(),
     HotToastModule.forRoot(),
     TippyModule.forRoot(),
@@ -42,13 +54,9 @@ import { ReactiveFormsModule } from "@angular/forms";
     CopyToClipboardModule,
     MatSidenavModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-  exports: [
-    FontAwesomeModule,
-    MatSidenavModule,
-    MatInputModule
-  ],
+  exports: [FontAwesomeModule, MatSidenavModule, MatInputModule],
   providers: [
     httpInterceptorProviders,
     {
@@ -64,9 +72,9 @@ import { ReactiveFormsModule } from "@angular/forms";
     {
       provide: NG_ENTITY_SERVICE_CONFIG,
       useValue: {
-        baseUrl: '/'
-      }
-    }
+        baseUrl: "/",
+      },
+    },
   ],
   bootstrap: [AppComponent],
 })
